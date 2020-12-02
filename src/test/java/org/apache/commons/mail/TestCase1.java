6a03569e46d7cbb1c2da544e616963524f25b318
Cary Inzerello <55421483+cinzerello@users.noreply.github.com>
2020-12-02 14:54:00 -0600

    Added Test Classes

---
+++ src/test/java/org/apache/commons/mail/TestCase1.java
@@ -0,0 +1,23 @@
+package org.apache.commons.mail;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TestCase1 extends TestCase{
+	Email testEmail;
+	public void setUp() {
+		testEmail = new SimpleEmail();
+	}
+	@Test
+	public void testAddCc() {
+		System.out.println("This test class tests the AddCc() function.");
+		// tests one parameter call
+		try {
+			testEmail.addCc("a@b.com");
+		} catch (EmailException e) {
+			fail();
+		}
+		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
+	}
+
+}
---
+++ src/test/java/org/apache/commons/mail/TestCase2.java
@@ -0,0 +1,23 @@
+package org.apache.commons.mail;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TestCase2 extends TestCase{
+	Email testEmail;
+	public void setUp() {
+		testEmail = new SimpleEmail();
+	}
+	@Test
+	public void testAddReplyTo() {
+		//when just email address is given
+		System.out.println("This test class tests the AddCc() function.");
+		try {
+			testEmail.addReplyTo("a@b.com");
+			assertEquals("a@b.com", testEmail.getReplyToAddresses().get(0).toString());
+		} catch (EmailException e) {
+			fail();
+		}
+	}
+
+}
